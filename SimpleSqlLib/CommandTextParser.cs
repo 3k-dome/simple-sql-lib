3d4239bc8c542e8d004bbf82b2f2f663002335c7
3k-dome <doenerfresser3k@gmail.com>
2022-08-04 19:44:02 +0200

    added a utility class to modify command texts

---
+++ SimpleSqlLib/CommandTextParser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Linq;
+
+namespace SimpleSqlLib {
+
+    public static class CommandTextParser {
+
+        public static string RemoveLastTerminator(string commandText) {
+            if (string.IsNullOrEmpty(commandText)) {
+                throw new ArgumentNullException(nameof(commandText));
+            }
+            if (commandText.Last() == ';') {
+                return commandText[..^1];
+            }
+            return commandText;
+        }
+
+        public static SqlCommand RemoveLastTerminator(SqlCommand command) {
+            command.CommandText = RemoveLastTerminator(command.CommandText);
+            return command;
+        }
+    }
+}
